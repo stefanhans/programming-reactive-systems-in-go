9e4aa77cc161e9a3d41de697c0d208f1cb193ace
“stefanhans” <uni@stefan-hans.de>
2019-05-24 15:11:17 +0200

    snippets for liner

---
+++ thesis-code-snippets/3.4.4/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/peterh/liner"
+)
+
+// Define a set of commands
+var commands = []string{"ls", "pwd", "date", "hostname", "quit"}
+
+func prompt() string {
+	return fmt.Sprintf("<%s %s> ", time.Now().Format("Jan 2 15:04:05.000"), "user")
+}
+
+func executeShellScript(cmdline string) bool {
+
+	if cmdline == "quit" {
+		os.Exit(0)
+	}
+
+	words := strings.Fields(cmdline)
+
+	if len(words) == 0 {
+		fmt.Print("")
+		return false
+	}
+
+	binary, lookErr := exec.LookPath(words[0])
+	if lookErr != nil {
+		fmt.Printf("%v\n", lookErr)
+		return false
+	}
+
+	var cmd *exec.Cmd
+	if len(words) == 1 {
+		cmd = exec.Command(binary)
+	} else {
+		fmt.Printf("command with arguments not supported\n")
+		return false
+	}
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Env = os.Environ()
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("could not run: %v\n", err)
+		return false
+	}
+
+	fmt.Printf(out.String())
+
+	return true
+}
+
+func main() {
+	s := liner.NewLiner()
+	s.SetTabCompletionStyle(liner.TabPrints)
+	s.SetCompleter(func(line string) (ret []string) {
+		for _, c := range commands {
+			if strings.HasPrefix(c, line) {
+				ret = append(ret, c)
+			}
+		}
+		return
+	})
+	defer s.Close()
+
+	for {
+		p, err := s.Prompt(prompt())
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
+		if executeShellScript(p) {
+			s.AppendHistory(p)
+		}
+	}
+}
